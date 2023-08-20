80ffdc2b87af251fb4d10ffa0a6e7d5ca4d67fa5
Brendon45 <jejebrendon722@gmail.com>
2023-08-20 08:35:57 -0700

    create main header file and bns_putchar file

---
+++ bns_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * bns_putchar - A function that prints one character at a time
+ * @c: A character to be printed
+ * Return: The size of c
+ */
+
+int bns_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef BNS_HEADER_FILE
+#define BNS_HEADER_FILE
+
+int _printf(const char *format, ...);
+int bns_putchar(char c);
+int bns_puts(char *str);
+
+#endif
