c5b468c1a2fd4559506e4f5da24b5df8e8c9c1b3
Simon Kachepa <kachepasimon@gmail.com>
2023-08-22 01:58:39 -0700

    Correct bns_printf.c

--- bns_printf.c
+++ bns_printf.c
@@ -12,13 +12,14 @@ int _printf(const char *format, ...)
 	int bns_index, bns_count = 0, bns_str_count;
 	va_list bns_list;
 
-	va_start(bns_list, format);
 	if (!format)
 		return (-1);
+	va_start(bns_list, format);
 	for (bns_index = 0; format[bns_index] != '\0'; bns_index++)
 	{
 		if (format[bns_index] != '%')
 			bns_putchar(format[bns_index]);
+			bns_count++;
 		else
 		{
 			if (format[bns_index + 1] == '\0')
@@ -27,23 +28,22 @@ int _printf(const char *format, ...)
 			{
 				case 'c':
 					bns_putchar(va_arg(bns_list, int));
-					bns_index++;
+					bns_index++, bns_count++;
 					break;
 				case 's':
 					bns_str_count = bns_puts(va_arg(bns_list, char *));
 					bns_index++;
-					bns_count = (bns_count + bns_str_count - 1);
+					bns_count = (bns_count + bns_str_count);
 					break;
 				case '%':
 					bns_putchar(format[bns_index + 1]);
-					bns_index++;
+					bns_index++, bns_count++;
 					break;
 				default:
 					bns_putchar(format[bns_index]);
-					bns_index++;
+					bns_index++, bns_count++;
 			}
 		}
-		bns_count++;
 	}
 	va_end(bns_list);
 	return (bns_count);
